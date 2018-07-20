8429559a72cdbcba0f3fed1aff6e63bee003c0d5
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-20 15:58:56 -0400

    Got it to kind of work

--- Capstone-Pig-Latin/Program.cs
+++ Capstone-Pig-Latin/Program.cs
@@ -10,15 +10,25 @@ namespace Capstone_Pig_Latin
     {
         static void Main(string[] args)
         {
+            string front = "", back = "";
+           char[] vowelList = new char[] { 'a', 'e', 'i', 'o', 'u' };
             Console.WriteLine("This will translate what you type into Pig Latin!");
            string userWord = Console.ReadLine();
-            string output = userWord.Substring((userWord.IndexOf('a')+1));
+            // string output = userWord.Substring((userWord.IndexOf('a')+1));
             if (userWord.StartsWith("a") || userWord.StartsWith("e") || userWord.StartsWith("i") || userWord.StartsWith("o") || userWord.StartsWith("u"))
             {
                 Console.WriteLine(userWord + "way");
                 Console.ReadKey();
             }
-            Console.WriteLine(output);
+            else for (int i = 0; i <= userWord.Length; i++)
+            {
+                    string[] words = userWord.Split(vowelList, 2);
+                    front = userWord.Substring(0,1);
+                    back = userWord.Substring(1);
+            }
+            Console.WriteLine(back + front + "ay");
+            Console.ReadKey();
+            //  Console.WriteLine(output);
             Console.ReadKey();
         }
     }
