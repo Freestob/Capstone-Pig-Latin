82f3a70164bd59fdb039bb6ba197eb18ff03deae
Freestob <bradley.freestone@gmail.com>
2018-07-22 21:24:05 -0400

    Added a do again

--- Capstone-Pig-Latin/Program.cs
+++ Capstone-Pig-Latin/Program.cs
@@ -10,27 +10,40 @@ namespace Capstone_Pig_Latin
     {
         static void Main(string[] args)
         {
-            //string front = "", back = "";
-           char[] vowelList = new char[] { 'a', 'e', 'i', 'o', 'u' };
-            Console.WriteLine("This will translate what you type into Pig Latin!");
-           string userWord = Console.ReadLine();
-            // string output = userWord.Substring((userWord.IndexOf('a')+1));
-            if (userWord.StartsWith("a") || userWord.StartsWith("e") || userWord.StartsWith("i") || userWord.StartsWith("o") || userWord.StartsWith("u"))
+            while (true)
             {
-                Console.WriteLine(userWord + "way");
-                Console.ReadKey();
+                Console.Clear();
+                char[] vowelList = new char[] { 'a', 'e', 'i', 'o', 'u' };
+                Console.WriteLine("This will translate what you type into Pig Latin!");
+                string userWord = Console.ReadLine().ToLower();
+
+                if (userWord.StartsWith("a") || userWord.StartsWith("e") || userWord.StartsWith("i") || userWord.StartsWith("o") || userWord.StartsWith("u"))
+                {
+                    Console.WriteLine(userWord + "way");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    int index = userWord.IndexOfAny(vowelList);
+                    userWord = userWord.Insert(index, " ");
+                    string[] temp = userWord.Split(' ');
+                    string back = temp[0];
+                    string front = temp[1];
+
+                    Console.WriteLine(front + back + "ay");
+                    Console.ReadLine();
+
+                }
+                Console.WriteLine("Would you like to translate another word?");
+
+                if (Console.ReadLine().ToLower() != "y")
+                {
+                    Console.WriteLine("Ankthay youway!");
+                    Console.ReadKey();
+                    break;
+                }
             }
-            else 
-            {
-                int index = userWord.IndexOfAny(vowelList);
-                userWord = userWord.Insert(index, " ");
-                string[] temp = userWord.Split(' ');
-                string back = temp[0];
-                string front = temp[1];
-                
-                Console.WriteLine(front + back + "ay");
-                Console.ReadLine();
-            }
+
         }
     }
 }
